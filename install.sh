a0d0b0b13d63965eb3257bd27622dbb3a91da315
Taylor Malloy <tmalloy@gmail.com>
2012-08-18 22:55:34 -0700

    Modified files to not include .git files

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 SCRIPT_DIR=`pwd`
 SCRIPT_NAME=`basename $0`
-FILES=`git ls-tree --name-only HEAD`
+FILES=`git ls-tree --name-only HEAD | grep -v .git`
 
 cd $HOME
 for FILE in $FILES; do
