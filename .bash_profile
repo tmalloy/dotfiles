e9c10c87a01d25994451f69fa59644d37515b465
Taylor Malloy <tmalloy@apple.com>
2014-01-31 16:16:26 -0800

    .git-completion script

--- .bash_profile
+++ .bash_profile
@@ -4,3 +4,8 @@ alias gst="git status"
 alias gca="git commita -a"
 alias gb="git branch"
 
+
+# Git auto completion
+if [ -f ~/.git-completion.bash ]; then
+    . ~/.git-completion.bash
+fi
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 SCRIPT_DIR=`pwd`
 SCRIPT_NAME=`basename $0`
 FILES=`git ls-tree --name-only HEAD | grep -v .git`
-FILES=$'.gitconfig\n'$FILES
+FILES=$'.gitconfig\n.git-completion.bash\n'$FILES
 
 cd $HOME
 for FILE in $FILES; do
