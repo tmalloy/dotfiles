5dbd80cccf8ab5cc90cfb6c594a5eef0f8c6f068
Taylor Malloy <tmalloy@apple.com>
2015-01-30 11:25:08 -0800

    New base-git-functions
    Some handy git functions for use in bash scripts

--- .bash_profile
+++ .bash_profile
@@ -2,6 +2,17 @@ export EDITOR='vim'
 
 alias fixairplay="sudo kill `ps -ax | grep 'coreaudiod' | grep 'sbin' |awk '{print $1}'`"
 
+# add our local bin to the path
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+export PATH=$PATH:$DIR/bin
+
+
 # fasd setup
 eval "$(fasd --init auto)"
 alias v='f -e vim'
---
+++ bin/base-git-functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+tm_git_stash()
+{
+    
+    TS=`date +"%s"`
+    git stash save "$TS - automated stash"
+
+}
+
+tm_git_stash_pop()
+{
+    VALID_SECONDS=10
+
+    # looks like the following line:
+    # stash@{0}: On master: 1422548744 - automated stash
+    STASH_TOP_MESSAGE=`git stash list | head -n1 | cut -f3 -d:`
+    NUMBER=`echo $STASH_TOP_MESSAGE | cut -f1 -d- | tr -d ' '`
+    MESSAGE=`echo $STASH_TOP_MESSAGE | cut -f2 -d-`
+
+    TS=`date +"%s"`
+    HOW_RECENT=`expr $TS - $NUMBER`
+    if [[ $HOW_RECENT -lt $VALID_SECONDS && $MESSAGE == *"automated stash"* ]]
+    then
+        echo "Popping stash"
+        git stash pop
+    else
+        echo "No automated stash within the last $VALID_SECONDS seconds"
+    fi
+
+}
+
+tm_git_current_branch()
+{
+    git branch | grep \* | cut -f2 -d' '
+}
+
