72c3eecb600010ced4efd678fc4885fe789d1e95
Taylor Malloy <mail@taylormalloy.com>
2023-08-14 14:57:17 +0200

    nvim

--- resources/files/.config/nvim/lua/plugins/lsp.lua
+++ resources/files/.config/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,7 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+        clojure_lsp = {},
       },
     },
   },
